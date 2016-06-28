e6fe4079bcd30517b85620dd63227d718f6431db
Jacopo Canton <jcanton@mech.kth.se>
2016-06-28 16:30:20 -0500

    added module for misc_stuff

---
+++ src_usr/misc_stuff.f
@@ -0,0 +1,13 @@
+      MODULE misc_stuff
+      !
+      ! Contains stuff that did not fit anywhere else
+      ! jcanton@mech.kth.se
+
+      implicit none
+
+
+!     CONTAINS
+!==============================================================================
+
+!==============================================================================
+      END MODULE misc_stuff
