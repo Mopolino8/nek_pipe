4b7bc97504712a80bd4d224d988eff1bc842e931
Jacopo Canton <jcanton@mech.kth.se>
2016-07-01 17:17:35 -0500

    added kinetic_energy to misc_stuff.f

--- src_usr/misc_stuff.f
+++ src_usr/misc_stuff.f
@@ -6,8 +6,37 @@
       implicit none
 
 
-!     CONTAINS
+      CONTAINS
 !==============================================================================
+
+      real function kinetic_energy(u, v, w)
+
+         implicit none
+
+         include 'SIZE_DEF'
+         include 'SIZE'
+         include 'MASS_DEF' ! bm1
+         include 'MASS'
+
+         real, intent(in) :: u(1), v(1), w(1)
+         integer :: n
+         real, dimension(:), allocatable :: tmp
+         real, external :: glsum ! math.f
+
+         n = nx1*ny1*nz1*nelv
+
+         allocate( tmp(n) )
+
+         call rzero(tmp, n)
+         call vdot3(tmp, u,v,w, u,v,w, n)
+         call col2(tmp, bm1, n)
+
+         kinetic_energy = 0.5*glsum(tmp, n)
+
+         deallocate( tmp )
+
+         return
+      end function kinetic_energy
 
 !==============================================================================
       END MODULE misc_stuff
